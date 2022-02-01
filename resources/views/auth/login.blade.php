7ec2d5bd8a3355e44d51b797abb229e0db0a0ed2
KFuee <jbisazac@icloud.com>
2022-02-01 16:01:11 +0100

    Borde primario y margen en tarjetas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
+            <div class="card border-primary mb-3">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
