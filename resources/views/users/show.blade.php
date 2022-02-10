78e42b94862f1b773601506ada67e9480948c9f9
KFuee <jbisazac@icloud.com>
2022-02-10 21:53:08 +0100

    Ocultadas reservas en mostrar usuario según rol

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,6 +7,7 @@
       <div class="card border-primary mb-3">
         @include('users.data')
 
+        @if (Auth::user()->isAdministrator())
         <!-- Muestra las sesiones de la actividad en una tabla -->
         <div class="card-header">Reservas del usuario</div>
 
@@ -18,6 +19,7 @@
         <div class="card-footer text-muted">
           Número de registros encontrados: {{ $appointments->count() }}
         </div>
+        @endif
         @endif
       </div>
     </div>
